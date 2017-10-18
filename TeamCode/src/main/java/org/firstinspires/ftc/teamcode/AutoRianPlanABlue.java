35267c1a3b02cdf8923d073aa307eb84f7af0bfe
haoning fu <hfu@apixio.com>
2017-10-17 22:11:14 -0700

    add AutoRainPlanABlue and PIDControl

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRianPlanABlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRianPlanABlue.java
@@ -52,7 +52,7 @@ import java.util.Locale;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list.
  */
-@Autonomous(name = "RianPlanABlue", group = "Rain")
+@Autonomous(name = "RianPlanABlue", group = "Rian")
 
 public class AutoRianPlanABlue extends OpMode {
 
